458c6c11b66fc2de3f96dff4ddb9f1a4856680ab
tarxton <89666132+tarxton@users.noreply.github.com>
2026-07-16 00:41:22 +0200

    lighting maps

--- src/lighting/fragment.glsl
+++ src/lighting/fragment.glsl
@@ -2,6 +2,7 @@
 
 in vec3 Normal;
 in vec3 FragPos;
+in vec2 TexCoords;
 
 out vec4 FragColor;
 
@@ -16,9 +17,9 @@ struct Light {
 uniform Light light;
 
 struct Material {
-    vec3 ambient;
-    vec3 diffuse;
-    vec3 specular;
+    sampler2D diffuse;
+    sampler2D specular;
+    sampler2D emission;
     float shininess;
 };
 
@@ -28,20 +29,22 @@ uniform vec3 viewPos;
 
 void main()
 {
-    vec3 ambient = light.ambient * material.ambient;
+    vec3 ambient = light.ambient * vec3(texture(material.diffuse, TexCoords));
 
     vec3 norm = normalize(Normal);
     vec3 lightDir = normalize(light.position - FragPos);
 
     float diff = max(dot(norm, lightDir), 0.0);
-    vec3 diffuse = light.diffuse * (diff * material.diffuse);
+    vec3 diffuse = light.diffuse * diff * vec3(texture(material.diffuse, TexCoords));
 
     vec3 viewDir = normalize(viewPos - FragPos);
     vec3 reflectDir = reflect(-lightDir, norm);
     float spec = pow(max(dot(viewDir, reflectDir), 0.0), material.shininess);
-    vec3 specular = light.specular * (spec * material.specular);
+    vec3 specular = light.specular * spec * vec3(texture(material.specular, TexCoords));
 
-    vec3 light = ambient + diffuse + specular;
+    vec3 emission = vec3(texture(material.emission, TexCoords));
+
+    vec3 light = ambient + diffuse + specular + emission;
 
     FragColor = vec4(light, 1.0);
 }
